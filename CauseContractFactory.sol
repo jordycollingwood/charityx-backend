8f1a6fdb29fe2e761e94bce3d844ba7240efe5af
jordycollingwood <88147719+jordycollingwood@users.noreply.github.com>
2023-04-19 15:20:19 +0100

    Update CauseContractFactory.sol
    get admin private key

--- CauseContractFactory.sol
+++ CauseContractFactory.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8;
 
 
 contract CauseContractFactory {
+
+    function getHash(uint256 passcode, uint256 nonce) external pure returns (bytes32){
+    return keccak256(abi.encode(choice, nonce));
+    }
     
 }
